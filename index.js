f71c31856e947caa49e19d6edabf9ef512acd208
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2018-12-28 09:45:27 -0600

    Initial Beta Release (#1)
    Added unit testing and corrected minor issues for initial beta release.

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ class BasicQueue {
     /**
      * Create a basic queue
      * @param {Array} initialQueue Initial array of values
+     * @param {Function} frozenCB Call back to execute when an op is run while queue is frozen
      */
-    constructor(initialQueue = []) {
+    constructor(initialQueue = [], frozenCB) {
         this._queue = Array.isArray(initialQueue) ? initialQueue : [];
         this._frozen = false;
-        this._frozenErrorMsg = {success: false, message: 'Queue is currently frozen, no ops allowed.'};
+        this._frozenCB = typeof frozenCB === 'function' ? frozenCB : null;
     };
 
     set queue(newQueue) {
@@ -21,57 +22,39 @@ class BasicQueue {
 
     get frozen() {
         return this._frozen;
-    }
+    };
 
-    /**
-     * Gets the next items in queue
-     * @param {Number} qty The quantity to be retruend starting from index 0
-     */
-    getNext(qty = 1) {
-        if(this._frozen)
-            return this._frozenErrorMsg;
+    set frozenCB(cb) {
+        if(typeof cb === 'function')
+            this._frozenCB = cb;
+    };
 
-        return this.getIndex(0, qty);
-    }
-
-    /**
-     * Gets the last item from the queue
-     * @param {Number} qty The quantity to be retruend
-     */
-    getLast(qty = 1) {
-        if(this._frozen)
-            return this._frozenErrorMsg;
-
-        return this.getIndex(-1, qty);
-    }
-
-    /**
-     * Take an item out of the queue at a specific index
-     * @param {Number} index The index to start at
-     * @param {Number} qty The number of records to retrieve
-     */
-    getIndex(index, qty = 1) {
-        if(this._frozen)
-            return this._frozenErrorMsg;
-
-        // If the index is the start or end no need to freeze queue
-        if(index === 0 || index === -1)
-            return this._queue.splice(index,qty);
-
-        this.freeze();
-        const items = this._queue.splice(index,qty);
-        this.unfreeze();
-        return items;
-    }
+    get frozenCB() {
+        return this._frozenCB;
+    };
 
     /** Freezes the queue */
     freeze() {
         this._frozen = true;
     };
-
+    
     /** Unfreeze the queue */
     unfreeze() {
         this._frozen = false;
+    };
+
+    /** Checks if frozen and executes CB if one is provided 
+     *  Internal FN designed for lib use
+     * @returns {Boolean} Indicates if it was frozen
+    */
+   freezeCheck() {
+        if(!this.frozen)
+            return false;
+
+        if(this.frozenCB)
+            this.frozenCB(this);
+        
+        return true;
     };
 
     /**
@@ -79,53 +62,87 @@ class BasicQueue {
      * @param {*} item Item to be added to queue
      */
     add(item) {
-        if(this._frozen)
-            return this._frozenErrorMsg;
+        if(this.freezeCheck())
+            return false;
 
         this._queue.push(item);
+        return true;
+    };
+
+    /**
+     * Add one item to the beginning of the queue
+     * @param {*} item Item to be added to queue
+     */
+    addToBeginning(item) {
+        if(this.freezeCheck())
+            return false;
+
+        this._queue.splice(0, 0, item);
         return {success: true, message: 'Added item to end of queue.'};
     };
 
     /**
-     * Add one item to the begnning of the queue
-     * @param {*} item Item to be added to queue
+     * Gets the next items in queue
+     * @param {Number} qty The quantity to be retruend starting from index 0
      */
-    addToBeginning(item) {
-        if(this._frozen)
-            return this._frozenErrorMsg;
+    getNext(qty = 1) {
+        if(this.freezeCheck())
+            return false;
 
-        this._queue.splice(1, 0, item);
-        return {success: true, message: 'Added item to end of queue.'};
+        return this.getIndex(0, qty);
+    };
+
+    /**
+     * Gets the last item from the queue
+     * @param {Number} qty The quantity to be retruend
+     */
+    getLast(qty = 1) {
+        if(this.freezeCheck())
+            return false;
+
+        return this.getIndex(qty * -1, qty).reverse();
+    };
+
+    /**
+     * Take an item out of the queue at a specific index
+     * @param {Number} index The index to start at
+     * @param {Number} qty The number of records to retrieve
+     */
+    getIndex(index = 0, qty = 1) {
+        if(this.freezeCheck())
+            return false;
+
+        return this._queue.splice(index,qty);
     };
 
     /**
      * Locates the item and removes it from the queue
      * @param {*} item Must be the EXACT item (===) comparison done
+     * @returns {Boolean} Indicates if the item was removed or not
      */
     remove(item) {
-        if(this._frozen)
-            return this._frozenErrorMsg;
+        if(this.freezeCheck())
+            return false;
 
         const index = this._queue.findIndex(qItem => qItem === item);
         if(index === -1)
-            return {success: false, message: 'Unable to locate item.'};
+            return false;
 
         return this.removeIndex(index);
-    }
+    };
 
     /**
      * Remove one item based on the array index
      * @param {Number} index Index of the item to be removed
      */
-    removeIndex(index) {
-        if(this._frozen)
-            return this._frozenErrorMsg;
+    removeIndex(index = 0, qty = 1) {
+        // Use get index, but only return if false
+        if(!this.getIndex(index, qty))
+            return false;
 
-        this.freeze();
-        this._queue.splice(index, 1);
-        this.unfreeze();
-        return {success: true, message: `Item removed from array at position ${index}.`};
+        // Return success
+        return true;
     };
-}
+};
 
-module.exports.BasicQueue = BasicQueue;
+module.exports = BasicQueue;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const BasicQueue = require('../index').BasicQueue;
+const BasicQueue = require('../index');
 
 describe('Basic Operations', function() {
     describe('#constructor()', function() {
@@ -11,6 +11,7 @@ describe('Basic Operations', function() {
             assert.strictEqual(new BasicQueue(['1']).queue[0], '1');
         });
     });
+
     describe('#getters & setters', function() {
         it('Should be able to get queue', function() {
             assert.strictEqual(Array.isArray(new BasicQueue().queue), true);
@@ -51,15 +52,69 @@ describe('Basic Operations', function() {
             q.freeze();
             assert.strictEqual(q.frozen, true);
         });
+        it('Should be able to set frozenCB', function() {
+            const testFn1 = () => {};
+            const testFn2 = () => {};
+
+            const q = new BasicQueue([], testFn1);
+            assert.strictEqual(q.frozenCB, testFn1);
+
+            q.frozenCB = testFn2;
+            assert.strictEqual(q.frozenCB, testFn2);
+        });
+        it('Should not be able to set frozenCB to anything, except a function', function() {
+            const q = new BasicQueue([], 'random string');
+            assert.strictEqual(q.frozenCB, null);
+
+            q.frozenCB = 'another string'
+            assert.strictEqual(q.frozenCB, null);
+
+            // Should stay as prev fn
+            const testFn1 = () => {};
+            q.frozenCB = testFn1;
+            assert.strictEqual(q.frozenCB, testFn1);
+            q.frozenCB = 'another string'
+            assert.strictEqual(q.frozenCB, testFn1);
+        });
+    });
+
+    describe('#freezeCheck()', function() {
+        it('Should execute CB if it exists and queue is frozen', function(done) {
+            const callDone = (theQ) => {
+                // We are expecting an error in this test, so if there is error return no error to mocha
+                if(theQ) {
+                    // The queue should be passed into the CB
+                    assert(theQ instanceof BasicQueue);
+                    assert.strictEqual(theQ.queue.length, 2)
+                    done();
+                    return
+                };
+
+                done(new Error('Expected an error'));
+            }
+
+            const q = new BasicQueue([1, 2], callDone);
+            q.freeze();
+            assert.strictEqual(q.add(1), false);
+        });
     });
 
     describe('#add()', function() {
-        it('Should have be able to add new items', function() {
+        it('Should return true when value is added', function() {
             const q = new BasicQueue();
-            q.add('1');
-            assert.strictEqual(q.queue[0], '1');
-            q.add('2');
-            assert.strictEqual(q.queue[1], '2');
+            assert.strictEqual(q.add(1), true);
+        });
+        it('Should return false if frozen', function() {
+            const q = new BasicQueue();
+            q.freeze();
+            assert.strictEqual(q.add(1), false);
+        });
+        it('Should be able to add new items', function() {
+            const q = new BasicQueue();
+            q.add(1);
+            assert.strictEqual(q.queue[0], 1);
+            q.add(2);
+            assert.strictEqual(q.queue[1], 2);
         });
         it('Should be able to add many types of items', function() {
             const q = new BasicQueue();
@@ -72,10 +127,104 @@ describe('Basic Operations', function() {
             assert.strictEqual(typeof q.queue[3], 'object');
         });
     });
+
+    describe('#addToBeginning()', function() {
+        it('Should be able to add new items to beginning of queue', function() {
+            const q = new BasicQueue();
+            assert.strictEqual(q.addToBeginning(1).success, true)
+            assert.strictEqual(q.queue[0], 1);
+            assert.strictEqual(q.addToBeginning(2).success, true)
+            assert.strictEqual(q.addToBeginning(3).success, true)
+            assert.strictEqual(q.addToBeginning(4).success, true)
+            assert.strictEqual(q.queue[0], 4);
+            assert.strictEqual(q.queue[3], 1);
+            assert.strictEqual(q.queue.length, 4);
+        });
+        // TODO: Add many to beginning
+        it('Should return false if frozen', function() {
+            const q = new BasicQueue();
+            q.freeze();
+            assert.strictEqual(q.addToBeginning(1), false);
+        });
+    });
+
+    describe('#getNext()', function() {
+        it('Should be able to get next item in queue', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            assert.strictEqual(q.getNext()[0], 1);
+            assert.strictEqual(q.queue.length, 2);
+            assert.strictEqual(q.queue[0], 2);
+        });
+        it('Should be able to get multiple next items in queue', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            const items = q.getNext(2);
+            assert.strictEqual(items[0], 1);
+            assert.strictEqual(items.length, 2);
+            assert.strictEqual(q.queue.length, 1);
+        });
+        it('Should return false if frozen', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            q.freeze();
+            assert.strictEqual(q.getNext(1), false);
+        });
+    });
+
+    describe('#getLast()', function() {
+        it('Should be able to get next last item in queue', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            assert.strictEqual(q.getLast()[0], 3);
+            assert.strictEqual(q.queue.length, 2);
+            assert.strictEqual(q.queue[0], 1);
+        });
+        it('Should be able to get multiple of the last items in queue', function() {
+            const q = new BasicQueue([1 , 2, 3, 4, 5, 6]);
+            const items = q.getLast(2);
+            assert.strictEqual(items[0], 6);
+            assert.strictEqual(items.length, 2);
+            assert.strictEqual(q.queue.length, 4);
+
+            const items2 = q.getLast(3);
+            assert.strictEqual(items2[0], 4);
+            assert.strictEqual(items2[2], 2);
+            assert.strictEqual(q.queue.length, 1);
+        });
+        it('Should return false if frozen', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            q.freeze();
+            assert.strictEqual(q.getLast(1), false);
+        });
+    });
+
+    describe('#getIndex()', function() {
+        it('Should default to index 0', function() {
+            const q = new BasicQueue([1 , 2, 3, 4, 5, 6, 7, 8]);
+            assert.strictEqual(q.getIndex()[0], 1); // Defaults to 0
+            assert.strictEqual(q.queue.length, 7); // Should have gone down by 1
+        });
+        it('Should be able to get an item at a specific index', function() {
+            const q = new BasicQueue([1 , 2, 3, 4, 5, 6, 7, 8]);
+            assert.strictEqual(q.getIndex(3)[0], 4); // Get index 3
+            assert.strictEqual(q.queue.length, 7); // Should have gone down by 1
+        });
+        it('Should be able to get multiple items starting at specific index', function() {
+            const q = new BasicQueue([1 , 2, 3, 4, 5, 6, 7, 8]);
+            const items = q.getIndex(2, 3);
+            assert.strictEqual(items[0], 3);
+            assert.strictEqual(items[2], 5);
+            assert.strictEqual(items.length, 3);
+            assert.strictEqual(q.queue.length, 5);
+        });
+        it('Should return false if frozen', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            q.freeze();
+            assert.strictEqual(q.getIndex(1), false);
+        });
+    });
+
     describe('#remove()', function() {
         it('Should be able to remove items', function() {
             const q = new BasicQueue([1 , 2, 3]);
-            assert.strictEqual(q.remove(2).success, true);
+            assert.strictEqual(q.remove(2), true);
             assert.strictEqual(q.queue[1], 3);
         });
         it('Should require exact equality to remove', function() {
@@ -83,12 +232,44 @@ describe('Basic Operations', function() {
             const q = new BasicQueue([exactObj, 2, '3']);
 
             // Try to remove similar obj
-            assert.strictEqual(q.remove({test: 'test'}).message, 'Unable to locate item.')
+            assert.strictEqual(q.remove({test: 'test'}), false)
             assert.strictEqual(q.queue[0], exactObj)
 
             // Try to remove exact obj
-            assert.strictEqual(q.remove(exactObj).success, true)
+            assert.strictEqual(q.remove(exactObj), true)
             assert.strictEqual(q.queue[0], 2)
+        });
+        it('Should return false if frozen', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            q.freeze();
+            assert.strictEqual(q.remove(1), false);
+        });
+    });
+
+    describe('#removeIndex()', function() {
+        it('Should default to index 0', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            assert.strictEqual(q.removeIndex(), true);
+            assert.strictEqual(q.queue[1], 3);
+            assert.strictEqual(q.queue.length, 2);
+        });
+        it('Should be able to delete specific index', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            assert.strictEqual(q.removeIndex(2), true);
+            assert.strictEqual(q.queue[1], 2);
+            assert.strictEqual(q.queue.length, 2);
+        });
+        it('Should be able to delete multiple items starting at a specific index', function() {
+            const q = new BasicQueue([1 , 2, 3, 4, 5]);
+            assert.strictEqual(q.removeIndex(2, 2), true);
+            assert.strictEqual(q.queue[1], 2);
+            assert.strictEqual(q.queue[2], 5);
+            assert.strictEqual(q.queue.length, 3);
+        });
+        it('Should return false if frozen', function() {
+            const q = new BasicQueue([1 , 2, 3]);
+            q.freeze();
+            assert.strictEqual(q.removeIndex(1), false);
         });
     });
   });
